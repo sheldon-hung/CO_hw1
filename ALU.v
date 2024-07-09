6b46fa1692f1f51b8653be741b450e25f0e9fee7
sheldon-hung <sheldon55071@gmail.com>
2024-07-09 23:18:42 +0800

    final version

---
+++ ALU.v
@@ -0,0 +1,15 @@
+module ALU( result, zero, overflow, aluSrc1, aluSrc2, invertA, invertB, operation );
+   
+  output wire[31:0] result;
+  output wire zero;
+  output wire overflow;
+
+  input wire[31:0] aluSrc1;
+  input wire[31:0] aluSrc2;
+  input wire invertA;
+  input wire invertB;
+  input wire[1:0] operation;
+  
+  /*your code here*/
+	  
+endmodule
